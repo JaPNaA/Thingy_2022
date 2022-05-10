9808ed9c90ba59566deddde466fe18e895ea2cb3
JaPNaA <leonehuang100@gmail.com>
2022-05-10 10:31:46 -0400

    Add ability to send text changes

--- liveChat/client/src/index.ts
+++ liveChat/client/src/index.ts
@@ -1,1 +1,55 @@
-document.body.innerHTML = "owo";
+import { TextFromDiffs } from "@shared/TextFromDiffs.js";
+
+const websocket = new WebSocket(location.origin.replace("http", "ws"));
+const textFromDiffs = new TextFromDiffs();
+
+websocket.addEventListener("open", function () {
+    websocket.send("clear");
+});
+
+websocket.addEventListener("message", function() {
+    //
+});
+
+const textarea = document.querySelector("textarea")!;
+
+let lastCursorSelectEnd = 0;
+let lastCursorSelectStart = 0;
+let lastTextLength = 0;
+
+textarea.addEventListener("beforeinput", function (event) {
+    lastCursorSelectEnd = textarea.selectionEnd;
+    lastCursorSelectStart = textarea.selectionStart;
+});
+
+textarea.addEventListener("input", function (event) {
+    const cursorPosition = textarea.selectionStart;
+    let editData;
+
+    if (lastCursorSelectEnd > lastCursorSelectStart) { // replacement
+        editData = {
+            position: lastCursorSelectStart,
+            endPosition: lastCursorSelectEnd,
+            text: textarea.value.slice(lastCursorSelectStart, cursorPosition)
+        };        
+    } else if (textarea.value.length > lastTextLength) { // insertion
+        editData = {
+            position: lastCursorSelectEnd,
+            endPosition: lastCursorSelectStart,
+            text: textarea.value.slice(lastCursorSelectEnd, cursorPosition)
+        };
+    } else { // deletion
+        editData = {
+            position: cursorPosition,
+            endPosition: lastCursorSelectEnd,
+            text: ""
+        };
+    }
+
+    if (editData) {
+        websocket.send("edit:" + JSON.stringify(editData));
+    }
+
+    lastCursorSelectEnd = cursorPosition;
+    lastTextLength = textarea.value.length;
+});
--- liveChat/server/src/main.ts
+++ liveChat/server/src/main.ts
@@ -1,6 +1,6 @@
-import { createServer } from 'https';
 import { WebSocketServer } from 'ws';
 import express from "express";
+import { TextFromDiffs } from './shared/TextFromDiffs';
 
 const expressApp = express();
 expressApp.use(express.static("../client/"));
@@ -9,10 +9,30 @@ expressApp.use(express.static("../client/"));
 const server = expressApp.listen(8080);
 const wsServer = new WebSocketServer({ server: server });
 
-wsServer.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
+wsServer.on('connection', function (ws) {
+    ws.on('message', function (data) {
         console.log('received: %s', data);
+        parseMessage(data.toString('utf8'));
     });
-
-    ws.send('something');
 });
+
+const currentText = new TextFromDiffs();
+
+function parseMessage(message: string) {
+    const commandSplitIndex = message.indexOf(":");
+    const command = commandSplitIndex < 0 ? message : message.slice(0, commandSplitIndex);
+    const dataText = commandSplitIndex < 0 ? "" : message.slice(commandSplitIndex + 1);
+
+    switch (command) {
+        case "edit": {
+            currentText.applyDiff(JSON.parse(dataText));
+            break;
+        }
+
+        case "clear":
+            currentText.clear();
+            break;
+    }
+
+    console.log(currentText);
+}
